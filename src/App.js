15e6d02918ed532ab6c4aa8d9b218561bb19aa25
Jozef <Jozefw@sbcglobal.net>
2022-04-17 08:00:23 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState,useReducer} from 'react';
+import Todo from './Todo.js';
 import './App.css';
 
+function reducer(state, action){
+  switch(action.type){
+    case 'increment':
+      return {count: state.count + 1}
+    case 'decrement':
+      return {count:state.count - 1}
+    default:
+      return {state}
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count:1})
+ 
+  const add = () =>{
+   dispatch({type:'increment'})
+  }
+  const minus = () =>{
+    dispatch({type:'decrement'})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={add}>+</button>
+      <span>{state.count}</span>
+      <button onClick={minus}>-</button>
+      <Todo></Todo>
     </div>
   );
 }
