96cb2b7155cdf2e171b3301164c2cacfa1a814e5
Jozef <Jozefw@sbcglobal.net>
2022-05-13 20:44:25 -0700

    add nothing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {useState,useReducer} from 'react';
-<<<<<<< HEAD
-import DoList from './DoList.js';
-import List from './List.js';
+
 import Listing from './Listing';
 import './App.css';
 
@@ -12,42 +10,6 @@ function App() {
     <div className="App">
     
       <Listing></Listing>
-=======
-import Todo from './DewList.js';
-import DewList from './DewList';
-import './App.css';
-
-
-function reducer(counter, action){
-  switch(action.type){
-    case 'increment':
-      return{count:counter.count+1}
-    case 'decrement':
-      return{count:counter.count-1}  
-    default:
-      
-      return counter  
-  }
-}
-
-function App() {
-  
- const [value, setValue] = useState(0);
- const [counter, dispatch] = useReducer(reducer,{count:0});
-
-  const add = () =>{
-   dispatch({type:'increment'})
-  }
-  const minus = () =>{
-    dispatch({type:'decrement'})
-  }
-  return (
-    <div className="App">
-      <button onClick={add}>+</button>
-      <span>Values:{counter.count}</span>
-      <button onClick={minus}>-</button>
-      <DewList></DewList>
->>>>>>> 5236d65292ea7554c43a6b8d569b7ff5abe11246
     </div>
   );
 }
